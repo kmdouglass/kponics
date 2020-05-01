6010e669f5f9b666fca28c5f7b351dc384798f27
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-05-01 09:57:09 +0200

    Add the kponics.com bucket
    In addition to adding the bucket, the group that administers the bucket now has
    restricted rights. Members may now only list, read, write, and delete objects.

--- infra/bootstrap/users_and_roles.tf
+++ infra/bootstrap/users_and_roles.tf
@@ -1,5 +1,32 @@
-locals {
-  s3_full_access_policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+data "aws_iam_policy_document" "kponics-bucket-admins-policy-document" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.kponics_bucket}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.kponics_bucket}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "kponics-bucket-admins-policy" {
+  name        = "kponics-bucket-admins-policy"
+  description = "Policy for reading, writing, and deleting files in the bucket hosting kponics.com"
+
+  policy = data.aws_iam_policy_document.kponics-bucket-admins-policy-document.json
 }
 
 resource "aws_iam_group" "bucket-admins" {
@@ -8,7 +35,7 @@ resource "aws_iam_group" "bucket-admins" {
 
 resource "aws_iam_group_policy_attachment" "bucket-admins-policy-attachment" {
   group      = aws_iam_group.bucket-admins.name
-  policy_arn = local.s3_full_access_policy_arn
+  policy_arn = aws_iam_policy.kponics-bucket-admins-policy.arn
 }
 
 resource "aws_iam_group_membership" "bucket-admins-membership" {
