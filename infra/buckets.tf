8944686018bf733a45fa16324fab4b3923dfcd99
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-05-01 11:10:45 +0200

    Serve the site from the bucket

--- infra/buckets.tf
+++ infra/buckets.tf
@@ -1,8 +1,33 @@
 resource "aws_s3_bucket" "kponics-bucket" {
   bucket = var.kponics_bucket
-  acl    = "private"
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.kponics-bucket-policy.json
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 
   tags = {
-    Name = "Kponics Bucket"
+    Name = "kponics.com Bucket"
+  }
+}
+
+data "aws_iam_policy_document" "kponics-bucket-policy" {
+  statement {
+    sid = "PublicReadGetObject"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.kponics_bucket}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
   }
 }
