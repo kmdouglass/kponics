afc5931022eec874eedf9bd7182a1ad21ad93ac4
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2020-05-01 09:59:26 +0200

    Rename admins to ops to de-emphasize permissions on the bucket

--- infra/bootstrap/users_and_roles.tf
+++ infra/bootstrap/users_and_roles.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "kponics-bucket-admins-policy-document" {
+data "aws_iam_policy_document" "kponics-bucket-ops-policy-document" {
   statement {
     actions = [
       "s3:ListBucket",
@@ -22,30 +22,30 @@ data "aws_iam_policy_document" "kponics-bucket-admins-policy-document" {
   }
 }
 
-resource "aws_iam_policy" "kponics-bucket-admins-policy" {
-  name        = "kponics-bucket-admins-policy"
+resource "aws_iam_policy" "kponics-bucket-ops-policy" {
+  name        = "kponics-bucket-ops-policy"
   description = "Policy for reading, writing, and deleting files in the bucket hosting kponics.com"
 
-  policy = data.aws_iam_policy_document.kponics-bucket-admins-policy-document.json
+  policy = data.aws_iam_policy_document.kponics-bucket-ops-policy-document.json
 }
 
-resource "aws_iam_group" "bucket-admins" {
-  name = "kponics-bucket-admins"
+resource "aws_iam_group" "bucket-ops" {
+  name = "kponics-bucket-ops"
 }
 
-resource "aws_iam_group_policy_attachment" "bucket-admins-policy-attachment" {
-  group      = aws_iam_group.bucket-admins.name
-  policy_arn = aws_iam_policy.kponics-bucket-admins-policy.arn
+resource "aws_iam_group_policy_attachment" "bucket-ops-policy-attachment" {
+  group      = aws_iam_group.bucket-ops.name
+  policy_arn = aws_iam_policy.kponics-bucket-ops-policy.arn
 }
 
-resource "aws_iam_group_membership" "bucket-admins-membership" {
-  name = "kponics-bucket-admins-membership"
+resource "aws_iam_group_membership" "bucket-ops-membership" {
+  name = "kponics-bucket-ops-membership"
 
   users = [
     "${aws_iam_user.kponics.name}",
   ]
 
-  group = aws_iam_group.bucket-admins.name
+  group = aws_iam_group.bucket-ops.name
 }
 
 resource "aws_iam_user" "kponics" {
